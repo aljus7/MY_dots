84f9269efca264b44e9cc1fade5389b908189b7c
aljus7 <86913447+aljus7@users.noreply.github.com>
2024-07-04 00:12:54 +0200

    Add files via upload

---
+++ .config/myScripts/gammastep_toggle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gammastep_statusjoza=$(pgrep gammastep) # Check if gammastep is running
+if [ ! -z "$gammastep_statusjoza" ]; then
+ # Terminate gammastep
+ pkill gammastep
+else
+ # Start gammastep
+ gammastep -O 4000 &
+fi
