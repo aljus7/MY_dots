15f1f33fd6b64141c0582daff7064b0b66fd1c5a
aljosa <mlinari.aljosa@gmail.com>
2025-09-13 00:07:20 +0200

    fixes

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo "###############################"
 
 echo "\nroot password needed (if not root)"
 sudo pacman -Syu --noconfirm
-sudo pacman -S util-linux hyprcursor hypridle hyprland hyprlang hyprlock hyprpaper hyprpicker hyprutils hyprwayland-scanner xdg-desktop-portal-hyprland kitty sddm polkit-kde-agent polkit uwsm grim slurp swappy tesseract cliphist wl-clipboard gammastep swaync rofi rofi-emoji waybar xorg-xwayland networkmanager sed awk pipewire pipewire-pulse wireplumber pavucontrol brightnessctl cups hplip firefox jq git base-devel cmake meson cpio --noconfirm && echo "Packages successfully installed!" || sleep 180
+sudo pacman -S util-linux hyprcursor hypridle hyprland hyprlang hyprlock hyprpaper hyprpicker hyprutils hyprwayland-scanner xdg-desktop-portal-hyprland kitty sddm polkit-kde-agent polkit uwsm grim slurp swappy tesseract cliphist wl-clipboard gammastep swaync rofi rofi-emoji waybar xorg-xwayland networkmanager sed awk pipewire pipewire-pulse wireplumber pavucontrol brightnessctl cups hplip firefox jq git base-devel cmake meson cpio libnotify bc qt5ct qt6ct --noconfirm && echo "Packages successfully installed!" || sleep 180
 
 echo ""
 echo "#######################################"
@@ -80,7 +80,7 @@ echo "###########################"
 echo "### Installing from AUR ###"
 echo "###########################"
 
-yay -S trivalent-bin aur/wallust-git --noconfirm
+yay -S trivalent-bin wallust-git --noconfirm
 
 echo ""
 echo "#######################################"
@@ -119,9 +119,12 @@ echo "#################################"
 
 cp -r ./config/* "$HOME/.config"
 
-cp ./wallpaper.png "$HOME/wallpaper"
+cp ./wallpaper.png "$HOME/wallpaper/"
 
 wallust run "$HOME/wallpaper" && "$HOME/.config/myScripts/update-colors.sh"
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
 echo ""
 echo "###############################"
