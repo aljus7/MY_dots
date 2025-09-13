8eb857eb0699bfca7d2366a11064ea7f22605232
aljosa <mlinari.aljosa@gmail.com>
2025-09-13 16:09:20 +0200

    some tweaks

--- install.sh
+++ install.sh
@@ -124,6 +124,7 @@ cp ./wallpaper.png "$HOME/wallpaper/wallpaper.png"
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.wm.preferences button-layout ''
 
 echo ""
 echo "###############################"
